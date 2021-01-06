64b40dc04e5e16a4aae1007067a72f4cf9bd6dd7
faraigonz <faraigonz@mail.com>
2021-01-05 19:43:49 -0500

    test

--- app/app.py
+++ app/app.py
@@ -19,12 +19,12 @@ def main():
 def index():
     return render_template('index.html')
     
-@app.route('/graph', methods=['POST'])
-def graph():
+@app.route('/plots', methods=['POST'])
+def plots():
 #    if request.method == 'POST':
         app.vars['ticker'] = request.form['ticker']
 	
-        api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json?api_key=gVz7XbzeecyxHdkCn8yB' % app.vars['ticker']
+        api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json?api_key=ues6Mm1_essC2iP-xMx6' % app.vars['ticker']
         session = requests.Session()
         session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
         raw_data = session.get(api_url)
@@ -48,7 +48,7 @@ def graph():
         if request.form.get('Adj. Open'):
             p.line(x=df['Date'].values, y=df['Adj. Open'].values,line_width=2, line_color="purple", legend='Adj. Open')
         script, div = components(p)
-        return render_template('graph.html', script=script, div=div)
+        return render_template('plots.html', script=script, div=div)
 
 if __name__ == '__main__':
     app.run(port=33507)
