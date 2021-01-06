59494fc734230631011a66cbda37374286f43b59
faraigonz <faraigonz@mail.com>
2021-01-06 13:03:37 -0500

    test

--- app/app.py
+++ app/app.py
@@ -1,62 +1,53 @@
-from flask import Flask, render_template, request, redirect
 import requests
-import pandas as pd
-import datetime
-import dateutil
-import re
+import pandas
 import simplejson as json
 from bokeh.plotting import figure
-from bokeh.embed import components
+from bokeh.palettes import Spectral11
+from bokeh.embed import components 
+from flask import Flask,render_template,request,redirect,session
 
 app = Flask(__name__)
 
+app.vars={}
+
+
 @app.route('/')
 def main():
-    return redirect('/index')
+  return redirect('/index')
 
-@app.route('/index',methods=['GET'])
-def userstockform():
+@app.route('/index', methods=['GET'])
+def index():
     return render_template('index.html')
+    
+@app.route('/graph', methods=['POST'])
+def graph():
+#    if request.method == 'POST':
+        app.vars['ticker'] = request.form['ticker']
+        
+        api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json?api_key=gVz7XbzeecyxHdkCn8yB' % app.vars['ticker']
+        session = requests.Session()
+        session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
+        raw_data = session.get(api_url)
 
-@app.route('/plots',methods=['POST'])
-def stockresults():
+        a = raw_data.json()
+        df = pandas.DataFrame(a['data'], columns=a['column_names'])
 
-    # Read user input from form
-    tickersymbol = request.form['tickersymbol']
+        df['Date'] = pandas.to_datetime(df['Date'])
 
-    # Get data and create dataframe
-    dataurl = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json?api_key=gVz7XbzeecyxHdkCn8yB' % tickersymbol
-    r = requests.get(dataurl)
-    dataset_df = pd.DataFrame(r.json())['dataset'].apply(pd.Series)
-    data = dataset_df.ix['data',:].apply(pd.Series)
-    data.columns = dataset_df.ix['column_names',0:12]
-    data['Date'] = pd.to_datetime(data['Date'])
-    today = datetime.date.today()
-    lastmonth = today - dateutil.relativedelta.relativedelta(months=1)
-    data_lastmonth = data[data['Date']>lastmonth]
-
-    name = re.search("(^.*\))",dataset_df.ix['name',0]).group(1)
-
-    p = figure(title=name,plot_width=600,plot_height=400,
-        x_axis_type="datetime",
-        x_range=(lastmonth,today),
-        x_axis_label = "Date",
-        y_axis_label = "USD")
-    if 'openingprice' in request.form:
-        p.line(data_lastmonth['Date'],data_lastmonth['Open'],line_color='blue',legend='Opening price')
-    if 'adjopeningprice' in request.form:
-        p.line(data_lastmonth['Date'],data_lastmonth['Adj. Open'],line_color='blue',line_dash=[4,4],legend='Adj. opening price')
-    if 'closingprice' in request.form:
-        p.line(data_lastmonth['Date'],data_lastmonth['Close'],line_color='red',legend='Closing price')
-    if 'adjclosingprice' in request.form:
-        p.line(data_lastmonth['Date'],data_lastmonth['Adj. Close'],line_color='red',line_dash=[4,4],legend='Adj. closing price')
-    p.legend.orientation = 'top_left'
-
-    script, div = components(p)
-
-    return render_template('plots.html',name=name, tickersymbol=tickersymbol, script=script, div=div)
-
-
+        p = figure(title='Stock prices for %s' % app.vars['ticker'],
+            x_axis_label='date',
+            x_axis_type='datetime')
+        
+        if request.form.get('Close'):
+            p.line(x=df['Date'].values, y=df['Close'].values,line_width=2, legend='Close')
+        if request.form.get('Adj. Close'):
+            p.line(x=df['Date'].values, y=df['Adj. Close'].values,line_width=2, line_color="green", legend='Adj. Close')
+        if request.form.get('Open'):
+            p.line(x=df['Date'].values, y=df['Open'].values,line_width=2, line_color="red", legend='Open')
+        if request.form.get('Adj. Open'):
+            p.line(x=df['Date'].values, y=df['Adj. Open'].values,line_width=2, line_color="purple", legend='Adj. Open')
+        script, div = components(p)
+        return render_template('graph.html', script=script, div=div)
 
 if __name__ == '__main__':
     app.run(port=33507)
