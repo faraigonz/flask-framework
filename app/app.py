3d7d6f743e0ffe837b259f0618901be77947e66e
faraigonz <faraigonz@mail.com>
2021-01-06 12:38:49 -0500

    test

--- app/app.py
+++ app/app.py
@@ -1,60 +1,62 @@
+from flask import Flask, render_template, request, redirect
 import requests
 import pandas as pd
-import simplejson as json
 import datetime
 import dateutil
+import re
+
 from bokeh.plotting import figure
-from bokeh.palettes import Spectral11
-from bokeh.embed import components 
-from flask import Flask,render_template,request,redirect,session
+from bokeh.embed import components
 
 app = Flask(__name__)
 
-app.vars={}
-
-
 @app.route('/')
 def main():
-  return redirect('/index')
+    return redirect('/userform')
 
-@app.route('/index', methods=['GET'])
-def index():
-    return render_template('index.html')
-    
-@app.route('/plots', methods=['POST'])
-def plots():
+@app.route('/userform',methods=['GET'])
+def userstockform():
+    return render_template('userstockform.html')
 
-        app.vars['ticker'] = request.form['ticker']
-	
-        stock_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json?api_key=ues6Mm1_essC2iP-xMx6' % app.vars['ticker']
-        #session = requests.Session()
-        #session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
-        
-	r = requests.get(stock_url)
-	data_df = pd.DataFrame(r.json())['dataset'].apply(pd.Series)
-        data = dataset_df.ix['data',:].apply(pd.Series)
-        data.columns = dataset_df.ix['column_names',0:12]
-        data['Date'] = pd.to_datetime(data['Date'])
+@app.route('/results',methods=['POST'])
+def stockresults():
 
-        #stock_df = pandas.DataFrame(data['data'], columns=data['column_names'])
-	current_date = datetime.date.today()
-	month = current_date - dateutil.relativedelta.relativedelta(months=1)
-	#stock_df['Date'] = pandas.to_datetime(stock_df['Date'])
-	month_data = stock_df[stock_df['Date']>month]
+    # Read user input from form
+    tickersymbol = request.form['tickersymbol']
 
-        p = figure(title='%s Stock Information' % app.vars['ticker'], x_axis_label='Date', x_axis_type='datetime',
-	x_range = (month, current_date), y_axis_label = "USD")
-        
-        if request.form.get('Close'):
-            p.line(x=month_data['Date'], y=month_data['Close'],line_width=2, legend='Close')
-        if request.form.get('Adj. Close'):
-            p.line(x=month_data['Date'], y=month_data['Adj. Close'].values,line_width=2, line_color="green", legend='Adj. Close')
-        if request.form.get('Open'):
-            p.line(x=month_data['Date'], y=month_data['Open'].values,line_width=2, line_color="red", legend='Open')
-        if request.form.get('Adj. Open'):
-            p.line(x=month_data['Date'], y=month_data['Adj. Open'].values,line_width=2, line_color="purple", legend='Adj. Open')
-        script, div = components(p)
-        return render_template('plots.html', script=script, div=div)
+    # Get data and create dataframe
+    dataurl = 'https://www.quandl.com/api/v3/datasets/WIKI/' + tickersymbol + '.json'
+    r = requests.get(dataurl)
+    dataset_df = pd.DataFrame(r.json())['dataset'].apply(pd.Series)
+    data = dataset_df.ix['data',:].apply(pd.Series)
+    data.columns = dataset_df.ix['column_names',0:12]
+    data['Date'] = pd.to_datetime(data['Date'])
+    today = datetime.date.today()
+    lastmonth = today - dateutil.relativedelta.relativedelta(months=1)
+    data_lastmonth = data[data['Date']>lastmonth]
+
+    name = re.search("(^.*\))",dataset_df.ix['name',0]).group(1)
+
+    p = figure(title=name,plot_width=600,plot_height=400,
+        x_axis_type="datetime",
+        x_range=(lastmonth,today),
+        x_axis_label = "Date",
+        y_axis_label = "USD")
+    if 'openingprice' in request.form:
+        p.line(data_lastmonth['Date'],data_lastmonth['Open'],line_color='blue',legend='Opening price')
+    if 'adjopeningprice' in request.form:
+        p.line(data_lastmonth['Date'],data_lastmonth['Adj. Open'],line_color='blue',line_dash=[4,4],legend='Adj. opening price')
+    if 'closingprice' in request.form:
+        p.line(data_lastmonth['Date'],data_lastmonth['Close'],line_color='red',legend='Closing price')
+    if 'adjclosingprice' in request.form:
+        p.line(data_lastmonth['Date'],data_lastmonth['Adj. Close'],line_color='red',line_dash=[4,4],legend='Adj. closing price')
+    p.legend.orientation = 'top_left'
+
+    script, div = components(p)
+
+    return render_template('results.html',name=name, tickersymbol=tickersymbol, script=script, div=div)
+
+
 
 if __name__ == '__main__':
     app.run(port=33507)
